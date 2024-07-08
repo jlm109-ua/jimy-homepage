cdde53212074b65d8eed5bc9c71b12b0579947c4
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 16:56:12 +0200

    Layout added to 404

--- pages/404.js
+++ pages/404.js
@@ -3,29 +3,32 @@ import { Container, Box, Heading, Text, Divider, Button } from '@chakra-ui/react
 import { useTranslation } from 'react-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
+import Layout from '../components/layouts/article'
 
 const NotFound = () => {
     const { t } = useTranslation('common')
 
     return (
-        <Container>
-            <Heading as="h1">
-                {t('not-found')}
-            </Heading>
-            <Text>
-                {t('not-found-desc')}
-            </Text>
+        <Layout title="404">
+            <Container>
+                <Heading as="h1">
+                    {t('not-found')}
+                </Heading>
+                <Text>
+                    {t('not-found-desc')}
+                </Text>
 
-            <Divider my={6} />
+                <Divider my={6} />
 
-            <Box my={6} align="center">
-                <NextLink href="/" passHref>
-                    <Button as="a" colorScheme="teal">
-                        {t('back-home')}
-                    </Button>
-                </NextLink>
-            </Box>
-        </Container>
+                <Box my={6} align="center">
+                    <NextLink href="/" passHref>
+                        <Button as="a" colorScheme="teal">
+                            {t('back-home')}
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Container>
+        </Layout>
     )
 }
 
