8d4a5d13ee1a635db526c1decab44e00fb66569b
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 21:57:22 +0200

    Icon fixes

--- components/icons/pizza.js
+++ components/icons/pizza.js
@@ -9,8 +9,8 @@ const PizzaIcon = props => (
         stroke-width="0.00024000000000000003"
         {...props}
     >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.144"></g>
+        <g id="SVGRepo_bgCarrier" stroke-width="0.00024000000000000003"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.00024000000000000003"></g>
         <g id="SVGRepo_iconCarrier"> 
             <path d="M18.512 9.60361C16.5667 8.57956 14.3509 8 11.9998 8C9.64864 8 7.43288 8.57956 5.48755 9.60362M9.99977 11H10.0098M12.9998 14H13.0098M11.9998 21L3.56396 6.23735C6.04988 4.81376 8.92979 4 11.9998 4C15.0697 4 17.9497 4.81376 20.4356 6.23735L11.9998 21Z" 
             stroke="#FFFFFF" stroke-width="0.00024000000000000003" stroke-linecap="round" stroke-linejoin="round">
