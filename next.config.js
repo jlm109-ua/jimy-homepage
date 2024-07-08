57f0c3fdf398271e22b659fde7e564e6160928c9
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 11:00:54 +0200

    Babel added

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
-const { webpack } = require('next/dist/compiled/webpack/webpack');
 const { i18n } = require('./next-i18next.config')
 const withTM = require('next-transpile-modules')(['puppeteer']);
 
 module.exports = withTM({
     reactStrictMode: true,
     i18n,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                fs: false,
-                net: false,
-                tls: false,
-                child_process: false,
-            };
-        }
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        });
 
         return config;
-    }
+    },
 });
