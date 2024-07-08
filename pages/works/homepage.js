ba94002e97633d0fdb46480275f4e1c644caefb2
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 18:47:34 +0200

    GitHub link added

--- pages/works/homepage.js
+++ pages/works/homepage.js
@@ -1,10 +1,11 @@
-import { Container, Badge } from '@chakra-ui/react'
+import { Container, Badge, ListItem } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { Title } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import useHasMounted  from '../../components/useHasMounted'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const Work = () => {
     const { t } = useTranslation('common')
@@ -24,6 +25,15 @@ const Work = () => {
                 <P>
                     {t('homepage-item-1-large')}
                 </P>
+
+                <List>
+                    <ListItem>
+                        <Meta>GitHub</Meta>
+                        <Link href="https://github.com/jlm109-ua/jimy-homepage">
+                        https://github.com/jlm109-ua/jimy-homepage <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                </List>
             </Container>
         </Layout>
     )
