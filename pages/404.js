0d133027bf4466b9a90f82ce5d6bf90928c44716
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 15:59:58 +0200

    Client-Server fix

--- pages/404.js
+++ pages/404.js
@@ -1,30 +1,37 @@
 import NextLink from 'next/link'
 import { Container, Box, Heading, Text, Divider, Button } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
+import React from 'react'
+import useHasMounted from '../components/useHasMounted'
 
 const NotFound = () => {
     const { t } = useTranslation('common')
+    const hasMounted = useHasMounted()
 
-    return (
-        <Container>
-            <Heading as="h1">
-                {t('not-found')}
-            </Heading>
-            <Text>
-                {t('not-found-desc')}
-            </Text>
+    if(!hasMounted) {
+        return null
+    } else {
+        return (
+            <Container>
+                <Heading as="h1">
+                    {t('not-found')}
+                </Heading>
+                <Text>
+                    {t('not-found-desc')}
+                </Text>
 
-            <Divider my={6} />
+                <Divider my={6} />
 
-            <Box my={6} align="center">
-                <NextLink href="/">
-                    <Button colorScheme='#008080'>
-                        {t('back-home')}
-                    </Button>
-                </NextLink>
-            </Box>
-        </Container>
-    )
+                <Box my={6} align="center">
+                    <NextLink href="/" passHref>
+                        <Button as="a" colorScheme="teal">
+                            {t('back-home')}
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Container>
+        )
+    }
 }
 
 export default NotFound
