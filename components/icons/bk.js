4987aca8335fb002f5015ccddcca69a19927690e
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 22:15:34 +0200

    Icon fixes

--- components/icons/bk.js
+++ components/icons/bk.js
@@ -6,15 +6,15 @@ const BurgerIcon = props => (
         viewBox="0 0 24 24" 
         xmlns="http://www.w3.org/2000/svg" 
         stroke="#FFFFFF" 
-        strokeWidth="0.00024000000000000003"
+        strokeWidth="2"
         {...props}
     >
         <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
         <g id="SVGRepo_iconCarrier">
-            <path stroke="#FFF" strokeLinejoin="round" strokeWidth="0.00024000000000000003" d="M12 2c-5.878 0-10.245 2.615-10.855 5.963C1.497 8.8 2.112 9.5 3 9.5h18c.888 0 1.503-.7 1.855-1.537C22.245 4.615 17.878 2 12 2Zm0 16c7.824 0 10.731-1.36 11.255-3.34.09-.357-.219-.66-.586-.66H3c-.367 0-.676.303-.586.66C1.269 16.64 4.176 18 12 18Z"></path>
-            <path stroke="#FFF" strokeLinecap="round" strokeWidth="0.00024000000000000003" d="M13.25 10.25v4.666m0 2.667v-2.667m0 0 2.333-1.25M19.75 10.25l-6.333 3.333m0 0L19.75 15.75"></path>
-            <path stroke="#FFF" strokeLinejoin="round" strokeWidth="0.00024000000000000003" d="M4.25 13h6.833c1.01 0 1.833.825 1.833 1.833s-.823 1.834-1.833 1.834H4.25v-3.667Zm0-3.666h5.833c1.01 0 1.833.825 1.833 1.833s-.823 1.833-1.833 1.833H4.25v-3.666Z"></path>
+            <path stroke="#FFF" strokeLinejoin="round" strokeWidth="2" d="M12 2c-5.878 0-10.245 2.615-10.855 5.963C1.497 8.8 2.112 9.5 3 9.5h18c.888 0 1.503-.7 1.855-1.537C22.245 4.615 17.878 2 12 2Zm0 16c7.824 0 10.731-1.36 11.255-3.34.09-.357-.219-.66-.586-.66H3c-.367 0-.676.303-.586.66C1.269 16.64 4.176 18 12 18Z"></path>
+            <path stroke="#FFF" strokeLinecap="round" strokeWidth="2" d="M13.25 10.25v4.666m0 2.667v-2.667m0 0 2.333-1.25M19.75 10.25l-6.333 3.333m0 0L19.75 15.75"></path>
+            <path stroke="#FFF" strokeLinejoin="round" strokeWidth="2" d="M4.25 13h6.833c1.01 0 1.833.825 1.833 1.833s-.823 1.834-1.833 1.834H4.25v-3.667Zm0-3.666h5.833c1.01 0 1.833.825 1.833 1.833s-.823 1.833-1.833 1.833H4.25v-3.666Z"></path>
         </g>
     </svg>
 );
