1d7e7cc9d2b4512629668ba00e4c1d1eb01e6f6d
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 10:48:23 +0200

    Trying to make the preview work

--- pages/api/generatePreview.js
+++ pages/api/generatePreview.js
@@ -1,26 +1,21 @@
 import puppeteer from "puppeteer";
 
-export default async (req, res) => {
+const handler = async (req, res) => {
     const { url } = req.query;
 
     if (!url) {
         return res.status(400).send('Missing URL query parameter');
     }
 
-    if (req.method === 'GET') {
-        try {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto('https://www.jimy-homepage.vercel.app', { waitUntil: 'networkidle2' });
-            const screenshot = await page.screenshot();
-            await browser.close();
-            res.setHeader('Content-Type', 'image/png');
-            res.end(screenshot);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Error generating screenshot');
-        }
-    } else {
-        res.status(405).send('Method not allowed');
-    }
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const screenshot = await page.screenshot({ encoding: 'base64' });
+
+    await browser.close();
+
+    res.status(200).json({ screenshot });
 }
+
+export default handler;
