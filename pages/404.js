39b3b356f992f79ff9e5acceeae6b4abcc0953c3
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 16:12:47 +0200

    Fixing 404

--- pages/404.js
+++ pages/404.js
@@ -1,37 +1,42 @@
 import NextLink from 'next/link'
 import { Container, Box, Heading, Text, Divider, Button } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
-import React from 'react'
+import React, { Suspense } from 'react'
 import useHasMounted from '../components/useHasMounted'
+
+const NotFoundContent = () => {
+    const { t } = useTranslation('common')
+
+    return (
+        <Container>
+            <Heading as="h1">
+                {t('not-found')}
+            </Heading>
+            <Text>
+                {t('not-found-desc')}
+            </Text>
+
+            <Divider my={6} />
+
+            <Box my={6} align="center">
+                <NextLink href="/" passHref>
+                    <Button as="a" colorScheme="teal">
+                        {t('back-home')}
+                    </Button>
+                </NextLink>
+            </Box>
+        </Container>
+    )
+}
 
 const NotFound = () => {
     const { t } = useTranslation('common')
-    const hasMounted = useHasMounted()
 
-    if(!hasMounted) {
-        return null
-    } else {
-        return (
-            <Container>
-                <Heading as="h1">
-                    {t('not-found')}
-                </Heading>
-                <Text>
-                    {t('not-found-desc')}
-                </Text>
-
-                <Divider my={6} />
-
-                <Box my={6} align="center">
-                    <NextLink href="/" passHref>
-                        <Button as="a" colorScheme="teal">
-                            {t('back-home')}
-                        </Button>
-                    </NextLink>
-                </Box>
-            </Container>
-        )
-    }
+    return (
+        <Suspense fallback={<div>{t('loading')}</div>}>
+            <NotFoundContent />
+        </Suspense>
+    )
 }
 
 export default NotFound
