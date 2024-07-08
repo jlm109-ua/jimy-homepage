a5c23321c3cbae39b34c08cdc9fa703c230ec2ef
jlm109-ua <jlm109@alu.ua.es>
2024-07-08 17:04:18 +0200

    Another S-C fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const { i18n } = require('./next-i18next.config');
-
 module.exports = {
-  reactStrictMode: true,
-  i18n
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
 };
